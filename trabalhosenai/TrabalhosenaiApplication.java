8a3426b17228b31bd88a8e0b92e76366001c662e
Maria Laura <marialaurasantosramosmaria@gmail.com>
2025-11-03 11:50:15 -0300

    Commit 1

---
+++ trabalhosenai/TrabalhosenaiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.infoa.trabalhosenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhosenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhosenaiApplication.class, args);
+	}
+
+}
